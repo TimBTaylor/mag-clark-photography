bb6e1dda6ae300561b87b15191bf804dcdb8de0e
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2022-01-11 22:17:54 -0500

    links layout

--- src/components/LandingLinks/LandingLinks.js
+++ src/components/LandingLinks/LandingLinks.js
@@ -1,6 +1,18 @@
 import React from "react";
 import "./LandingLinks.css";
+import FacebookLogo from "../../images/facebookLogo.svg";
+import InstagramLogo from "../../images/instagramLogo.svg";
 
 export const LandingLinks = () => {
-  return <div className="LandingLinksContainer"></div>;
+  return (
+    <div className="LandingLinksContainer">
+      <div className="btnContainer">
+        <div className="backgroundContainer"></div>
+        <button className="btn">pricing</button>
+        <button className="btn">portfolio</button>
+        <button className="btn">about</button>
+        <button className="btn">contact</button>
+      </div>
+    </div>
+  );
 };
