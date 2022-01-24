fc0bfe6b193a9b2055fe7af75aa9fceed4dbf0e1
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2022-01-24 17:43:44 -0500

    about page responsiveness

--- src/components/About/AboutBody.js
+++ src/components/About/AboutBody.js
@@ -5,16 +5,16 @@ import Portrait from "../../images/portrait.png";
 export const AboutBody = () => {
   return (
     <div className="aboutBodyContainer">
-      <img src={Portrait} alt="portait" className="portrait" />
       <div className="aboutTextContainer">
+        <img src={Portrait} alt="portait" className="portrait" />
         <h1 className="welcome">Welcome!</h1>
-        <h1 className="text">
+        <p className="text">
           my name is maggie. I am 22 years old. I graduated from GRCC in 2020
           with an associates degree unsure of my next steps. My lovely fiancé
           purchased me a camera and i started taking courses in 2021. I am so
           passionate about learning the art of photographty and thankful you are
           here!
-        </h1>
+        </p>
       </div>
     </div>
   );
