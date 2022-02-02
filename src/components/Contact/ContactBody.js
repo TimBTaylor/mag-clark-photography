875f501828da117116769c448b158437724da3f6
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2022-02-01 19:41:14 -0500

    emailjs install and setup

--- src/components/Contact/ContactBody.js
+++ src/components/Contact/ContactBody.js
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useRef } from "react";
 import "../../styles/Contact/ContactStyles.css";
 import Instagram from "../../images/instagram-contact.svg";
 import Facebook from "../../images/facebook-contact.svg";
 import Email from "../../images/email-contact.svg";
+import Portrait from "../../images/portrait.png";
+import emailjs from "@emailjs/browser";
 
 export const ContactBody = () => {
-  return <div className="contactBodyContainer"></div>;
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_67jhdew",
+        "YOUR_TEMPLATE_ID",
+        form.current,
+        "YOUR_USER_ID"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+  return (
+    <div className="contactContainer">
+      <div className="contactTextContainer">
+        <img src={Portrait} alt="portait" className="portrait" />
+        <form ref={form} onSubmit={sendEmail}>
+          <label>Name</label>
+          <input type="text" name="user_name" />
+          <label>Email</label>
+          <input type="email" name="user_email" />
+          <label>Message</label>
+          <textarea name="message" />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    </div>
+  );
 };
