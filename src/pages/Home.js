f6ac0ad5a3dfa83036ac65d746de39c1fa832094
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2022-03-10 22:58:22 -0500

    navbar updates and intro

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,7 @@ import "../styles/Home/HomeStyles.css";
 export const Home = () => {
   return (
     <div className="homeContainer">
+      <h1 className="intro">capturing memories for a lifetime</h1>
       <div className="headerAndImage">
         <Header title="home" />
       </div>
