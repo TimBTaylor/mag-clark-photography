e469da86de80fe556c69f34e9bf116735327e873
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2022-03-17 21:46:08 -0400

    meet me redesign

--- src/components/MeetMe/MeetMe.js
+++ src/components/MeetMe/MeetMe.js
@@ -5,25 +5,18 @@ import "../../styles/MeetMe/MeetMe.css";
 export const MeetMe = () => {
   return (
     <div className="meetMeContainer">
-      <div className="portraitContainer">
-        <p className="meetMe">meet me</p>
-        <img
-          src={maggiePortrait}
-          className="maggiePortrait"
-          alt="maggie portrait"
-        />
-      </div>
       <div className="meetMeTextContainer">
+        <p className="meetMe">meet me</p>
         <p className="meetMeText">
-          <span className="myName">Hello I'm maggie!</span> I am 22 years old. I
-          graduated from GRCC in 2020 with an associates degree unsure of my
+          <span className="myName">Hello, I'm maggie!</span> I am 22 years old.
+          I graduated from GRCC in 2020 with an associates degree unsure of my
           next steps. My lovely fiancé purchased me a camera and i started
-          taking courses in 2021.
+          taking courses in 2021. I am so passionate about learning the art of
+          photography and thankful you are here!
         </p>
-        <p className="passionate">
-          I am so passionate about learning the art of photography and thankful
-          you are here!
-        </p>
+      </div>
+      <div className="meetImgContainer">
+        <img src={maggiePortrait} className="maggiePortrait" alt="portrait" />
       </div>
     </div>
   );
