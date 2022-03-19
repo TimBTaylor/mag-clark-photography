e2a8b7cdb2a1bb174f75643b6611fad390a94308
TimBTaylor <79194271+TimBTaylor@users.noreply.github.com>
2022-03-18 21:09:32 -0400

    scroll to top button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,14 +4,27 @@ import { Header } from "../components/Header/Header";
 import { PhotoCollage } from "../components/PhotoCollage/PhotoCollage";
 import { MeetMe } from "../components/MeetMe/MeetMe";
 import { Investment } from "../components/Investment/Investment";
+import { BsFillArrowUpCircleFill } from "react-icons/bs";
 
 export const Home = () => {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+    console.log("clickled");
+  };
+
   return (
     <div className="homeContainer">
       <h1 className="intro">capturing memories for a lifetime</h1>
       <div className="headerAndImage">
-        <Header title="home" />
+        <Header />
       </div>
+      <button className="scrollToTop" onClick={scrollToTop}>
+        <BsFillArrowUpCircleFill className="arrowUp" />
+      </button>
       <PhotoCollage />
       <MeetMe />
       <Investment />
